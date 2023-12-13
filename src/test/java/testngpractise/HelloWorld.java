8c26b1ff44683df6f9db5f500e1dd6cc8637fcbd
sravani <sravani@s>
2023-12-13 11:03:33 +0530

    this is my 2nd commit by sravani through branch

---
+++ src/test/java/testngpractise/HelloWorld.java
@@ -0,0 +1,9 @@
+package testngpractise;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello sravani world ");	}
+
+}
